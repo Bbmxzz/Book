0d49153583ee53ef6e89b9032a8fe0deb584cf7a
Bbmxzz <pichayapa1147@gmail.com>
2024-10-12 14:03:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ from pythainlp import correct
 from pythainlp import word_tokenize
 
 corrections_file = "corrections.txt"
+port = int(os.environ.get('PORT', 5000))
 
 app = Flask(__name__)
 rf = Roboflow(api_key="GjIhJ9A525bYsGiVQIRA")
@@ -140,4 +141,4 @@ def correct_ocr_result(proc, corrections):
     return "".join(corrected_results)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=port)
